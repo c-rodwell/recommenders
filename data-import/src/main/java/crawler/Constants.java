99b2b11c3272b93f4580ac778a66d8c5c9bc8bc3
trishafuntanilla <trishafuntanilla@gmail.com>
2018-05-28 23:36:08 -0700

    test create track vectors

---
+++ data-import/src/main/java/crawler/Constants.java
@@ -0,0 +1,18 @@
+package crawler;
+
+public class Constants {
+
+    public static final String ES_HOST = "localhost";
+    public static final int ES_PORT = 9200;
+    public static final String SCHEME = "http";
+
+    public static final String USERS_INDEX = "users";
+    public static final String USERS_TYPE = "user";
+
+    public static final String TRACK_VECTORS_INDEX = "trackvectors";
+    public static final String TRACK_VECTORS_TYPE = "tvec";
+
+    public static final String USERS_FILE = "users-small.json";
+    public static final String APIKey = "685a323d182636518e80a296f620c8a2";
+
+}
---
+++ data-import/src/main/java/crawler/TrackVectors.java
@@ -0,0 +1,66 @@
+package crawler;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.apache.log4j.Logger;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TrackVectors {
+
+    private static final Logger LOG = Logger.getLogger(TrackVectors.class);
+
+    public static void createTrackVectors() throws IOException {
+
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.size(0);
+        TermsAggregationBuilder aggregationBuilder =
+                AggregationBuilders.terms("unique_tracks").field("track_mid").size(10);
+        builder.aggregation(aggregationBuilder);
+
+        SearchRequest request = new SearchRequest(Constants.USERS_INDEX);
+        request.source(builder);
+        SearchResponse response = HighClient.getInstance().getClient().search(request);
+
+        Aggregations aggr = response.getAggregations();
+        Terms resp_terms = aggr.get("unique_tracks");
+
+        int docId = 0;
+        for (Terms.Bucket b: resp_terms.getBuckets()){
+            String trackMid = b.getKeyAsString();
+            System.out.println(trackMid);
+
+            QueryBuilder queryBuilder = QueryBuilders.matchQuery("track_mid", trackMid);
+            SearchRequest request2 = new SearchRequest(Constants.USERS_INDEX);
+            request2.source(new SearchSourceBuilder().query(queryBuilder));
+            SearchResponse response2 = HighClient.getInstance().getClient().search(request2);
+            SearchHits hits = response2.getHits();
+            JsonArray vector = new JsonArray();
+            for (SearchHit hit : hits) {
+                int playCount = (int) hit.getSourceAsMap().get("track_playcount");
+                System.out.println(playCount);
+                vector.add(playCount);
+            }
+            JsonObject esObj = new JsonObject();
+            esObj.addProperty("track_mid", trackMid);
+            esObj.add("vector", vector);
+            HighClient.getInstance().postJsonToES(Constants.TRACK_VECTORS_INDEX, Constants.TRACK_VECTORS_TYPE, docId, esObj);
+            docId++;
+        }
+
+
+    }
+
+}
