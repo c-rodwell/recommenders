71c2815435c1e868db2887dd8599fa24da3afa6b
Colin <rodwell.colin@gmail.com>
2018-06-02 00:20:27 -0700

    write history to ES as json

--- data-import/src/main/java/crawler/UserHistory.java
+++ data-import/src/main/java/crawler/UserHistory.java
@@ -1,5 +1,6 @@
 package crawler;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import de.umass.lastfm.PaginatedResult;
 import de.umass.lastfm.Track;
@@ -24,32 +25,53 @@ public class UserHistory {
     public static void makeUserHistories() throws IOException {
 
         //create an index for user histories, but for now I'll do it as a hashmap
-        HashMap<String, ArrayList<ArrayList<String>>> allUsersHistories = new HashMap<String, ArrayList<ArrayList<String>>>();
+        //HashMap<String, ArrayList<ArrayList<String>>> allUsersHistories = new HashMap<String, ArrayList<ArrayList<String>>>();
 
         //I made TrackVectors.UniqueUsers() public so I can use it here
         //should it go in a utility methods class instead? or save the info in an accessible place?
         Terms uniqueUsersTerms = TrackVectors.getUniqueUsers();
         int docId=0;
         for (Terms.Bucket b : uniqueUsersTerms.getBuckets()) {
-            ArrayList<ArrayList<String>> userHistories = new ArrayList<ArrayList<String>>(); //should there be a class for this?
+
+            //what form to put a vector in ES? Trackvectors has JsonArray(), TagSimVectors does Gson().toJsonTree on an Arraylist
+            //try a json with explicit integers as keys, maybe that will be easier to retreive
+
+            //ArrayList<ArrayList<String>> userHistories = new ArrayList<ArrayList<String>>(); //should there be a class for this?
+            JsonObject userHistoriesObj = new JsonObject();
+
             String username = b.getKeyAsString();
             PaginatedResult<Track> recentTracksPage = User.getRecentTracks(username,1, recentTracksSize, Constants.APIKey);
             Iterator<Track> trackIter = recentTracksPage.iterator();;
-            for (int i=0; i<historiesPerUser; i++){
-                ArrayList<String> currentHistory = new ArrayList<String>();
-                for (int j=0; j<historySize; j++) {
+            for (int i=1; i<=historiesPerUser; i++){
+                //ArrayList<String> currentHistory = new ArrayList<String>();
+                JsonObject currentHistoryObj = new JsonObject();
+                for (int j=1; j<=historySize; j++) {
+                    if (!trackIter.hasNext()){//out of tracks - how to handle it when user history is smaller than others?
+                        System.out.println("user \""+username+"\" ran out of track history at history ="+i+", track = "+j);
+                        break;
+                    }
                     Track t = trackIter.next();
-                    currentHistory.add(t.getMbid());
+
+                    //currentHistory.add(t.getMbid());
+                    String mbid = t.getMbid();
+                    //only count non-blank mbid. if all are blank, we should eventually hit the "trackIter is out" condition
+                    if (mbid.equals("")){
+                        j--; //repeat this index
+                    }
+                    else {
+                        currentHistoryObj.addProperty(Integer.toString(j), mbid); //use int -> string as key, will that be easy to retrieve?
+                    }
                 }
-                userHistories.add(currentHistory);
+                //userHistories.add(currentHistory);
+                userHistoriesObj.add(Integer.toString(i), currentHistoryObj);
             }
             //save history for that user in ES (hashmap for now) - what structure should it go in?
-            allUsersHistories.put(username,userHistories);
+            //allUsersHistories.put(username,userHistories);
             JsonObject esObj = new JsonObject();
             esObj.addProperty("username", username);
-            JsonObject historiesObj = new JsonObject();
-            historiesObj.addProperty("test", 0);  //replace 0 with some representation of the histories
-            esObj.add("histories", historiesObj);
+            //JsonObject historiesObj = new JsonObject();
+            //historiesObj.addProperty("test", 0);  //replace 0 with some representation of the histories
+            esObj.add("histories", userHistoriesObj);
             HighClient.getInstance().postJsonToES(Constants.HISTORY_INDEX, Constants.HISTORY_TYPE, docId, esObj);
             docId++;
         }
